ffa3dc5e642bfe66d5e3cbfed3f2f03871556904
JonatanHvidberg <jonatan@eksperimentrum.dk>
2022-08-04 10:33:53 +0200

    test

--- trainer/model_utils.py
+++ trainer/model_utils.py
@@ -184,7 +184,9 @@ def unet_segment(cnn, image, bs, in_w, out_w, threshold=0.5):
         for _ in range(bs):
             if tile_idx < len(tiles):
                 tile = tiles[tile_idx]
+                print(tile)
                 tile = img_as_float32(tile)
+                print(tile)
                 tile = im_utils.normalize_tile(tile)
                 tile = np.moveaxis(tile, -1, 0)
                 tile_idx += 1
