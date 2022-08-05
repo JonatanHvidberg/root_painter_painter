287b6d9892469d351e7d4a8f037a7998bbbf09bf
JonatanHvidberg <jonatan@eksperimentrum.dk>
2022-08-05 13:26:48 +0200

    add models_models and som test of sheap

--- trainer/model_utils.py
+++ trainer/model_utils.py
@@ -171,6 +171,9 @@ def unet_segment(cnn, image, bs, in_w, out_w, threshold=0.5):
     """
     Threshold set to None means probabilities returned without thresholding.
     """
+    print('Shape(image)')
+    print(Shape(image))
+    
     assert image.shape[0] >= in_w, str(image.shape[0])
     assert image.shape[1] >= in_w, str(image.shape[1])
 
@@ -184,9 +187,9 @@ def unet_segment(cnn, image, bs, in_w, out_w, threshold=0.5):
         for _ in range(bs):
             if tile_idx < len(tiles):
                 tile = tiles[tile_idx]
-                print(tile)
+                print('Shape(tile)')
+                print(Shape(tile))
                 tile = img_as_float32(tile)
-                print(tile)
                 tile = im_utils.normalize_tile(tile)
                 tile = np.moveaxis(tile, -1, 0)
                 tile_idx += 1
---
+++ trainer/models_model.py
@@ -0,0 +1,6 @@
+
+
+
+
+
+import model_utils
