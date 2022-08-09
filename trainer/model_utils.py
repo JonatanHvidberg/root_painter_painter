2921a119c7fe2a8623764b929dd3d7de9c0160aa
JonatanHvidberg <jonatan@eksperimentrum.dk>
2022-08-09 11:03:25 +0200

    fist seg test 5

--- trainer/model_utils.py
+++ trainer/model_utils.py
@@ -171,8 +171,6 @@ def unet_segment(cnn, image, bs, in_w, out_w, threshold=0.5):
     """
     Threshold set to None means probabilities returned without thresholding.
     """
-    print('Shape(image)')
-    print(np.Shape(image))
 
     assert image.shape[0] >= in_w, str(image.shape[0])
     assert image.shape[1] >= in_w, str(image.shape[1])
@@ -187,8 +185,8 @@ def unet_segment(cnn, image, bs, in_w, out_w, threshold=0.5):
         for _ in range(bs):
             if tile_idx < len(tiles):
                 tile = tiles[tile_idx]
-                print('Shape(tile)')
-                print(np.Shape(tile))
+                print('shape(tile)')
+                print(np.shape(tile))
                 tile = img_as_float32(tile)
                 tile = im_utils.normalize_tile(tile)
                 tile = np.moveaxis(tile, -1, 0)
