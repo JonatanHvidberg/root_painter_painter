bc6af61b9e1b84119335062636adebbd9144ff34
JonatanHvidberg <jonatan@eksperimentrum.dk>
2022-08-31 12:37:26 +0200

    refactory and set op new data

--- trainer/models_model.py
+++ trainer/models_model.py
@@ -1,67 +1,35 @@
 
 
+import models_model_lib  as mml
 import model_utils
-import im_utils
-from unet2 import UNetGNRes
 from datasets2 import TrainDataset as TrainDataset2
-import models_model_lib 
+import im_utils
 
-import multiprocessing
-
-import time
-import os
 import numpy as np
-from skimage.io import imread, imsave
-from skimage import color, img_as_float32
-import torch
-from torch.nn.functional import softmax
-from loss import combined_loss as criterion
-from torch.utils.data import DataLoader
 
-from model_utils import save_if_better
 
-from file_utils import ls
+def setop()
+    pass
 
-from functools import partial
-import copy
-'''
-from model_utils thens to mage my model
-====================================================================================================
-'''
-def create_first_model_with_random_weights(model_dir):
-    # used when no model was specified on project creation.
-    
-    model_num = 1
-    model_name = str(model_num).zfill(6)
-    model_name += '_' + str(int(round(time.time()))) + '.pkl'
-    model = UNetGNRes()
-    model = torch.nn.DataParallel(model)
-    model_path = os.path.join(model_dir, model_name)
-    torch.save(model.state_dict(), model_path)
-    model.cuda()
-    return model
+def train_type2(model_path, train_annot_dir, dataset_dir):
+    train_set = TrainDataset2(train_annot_dir,dataset_dir,in_w,out_w)
 
-def load_model(model_path):
-    model = UNetGNRes()
-    try:
-        model.load_state_dict(torch.load(model_path))
-        model = torch.nn.DataParallel(model)
-    except:
-        model = torch.nn.DataParallel(model)
-        model.load_state_dict(torch.load(model_path))
-    model.cuda()
-    return model
+    model = mml.load_model(model_path)
 
-def ensemble_segment(model_paths, image, bs, in_w, out_w,
-                     threshold=0.5):
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.99, nesterov=True)
+
+    mml.train_epoch(train_set, model, optimizer)    
+    pass
+
+def segment_gradian(model_paths, image, bs, in_w, out_w):
     """ Average predictions from each model specified in model_paths """
     pred_sum = None
     pred_count = 0
     # then add predictions from the previous models to form an ensemble
     for model_path in model_paths:
-        cnn = load_model(model_path)
-
-        preds = unet_segment(cnn, image, bs, in_w, out_w, threshold=None)
+        cnn = model_utils.load_model(model_path)
+        preds = model_utils.unet_segment(cnn, image,
+                             bs, in_w, out_w, threshold=None)
         if pred_sum is not None:
             pred_sum += preds
         else:
@@ -69,312 +37,26 @@ def ensemble_segment(model_paths, image, bs, in_w, out_w,
         pred_count += 1
         # get flipped version too (test time augmentation)
         flipped_im = np.fliplr(image)
-        flipped_pred = unet_segment(cnn, flipped_im, bs, in_w,
+        flipped_pred = model_utils.unet_segment(cnn, flipped_im, bs, in_w,
                                     out_w, threshold=None)
         pred_sum += np.fliplr(flipped_pred)
         pred_count += 1
     foreground_probs = pred_sum / pred_count
-    predicted = foreground_probs > threshold
-    predicted = predicted.astype(int)
-    return predicted
+    foreground_probs = foreground_probs.astype(int)
+    #predicted = foreground_probs > threshold
+    #predicted = predicted.astype(int)
+    return foreground_probs
 
-
-def unet_segment(cnn, image, bs, in_w, out_w, threshold=0.5):
-    """
-    Threshold set to None means probabilities returned without thresholding.
-    """
-
-    assert image.shape[0] >= in_w, str(image.shape[0])
-    assert image.shape[1] >= in_w, str(image.shape[1])
-
-    tiles, coords = im_utils.get_tiles(image,
-                                       in_tile_shape=(in_w, in_w, 4),
-                                       out_tile_shape=(out_w, out_w))
-    tile_idx = 0
-    batches = []
-    while tile_idx < len(tiles):
-        tiles_to_process = []
-        for _ in range(bs):
-            if tile_idx < len(tiles):
-                tile = tiles[tile_idx]
-                #tile = img_as_float32(tile)
-                tile = im_utils.normalize_tile(tile)
-                #print(tile)
-                tile = np.moveaxis(tile, -1, 0)
-                tile_idx += 1
-                tiles_to_process.append(tile)
-        tiles_for_gpu = torch.from_numpy(np.array(tiles_to_process))
-        tiles_for_gpu.cuda()
-        tiles_for_gpu = tiles_for_gpu.float()
-        batches.append(tiles_for_gpu)
-
-    output_tiles = []
-    for gpu_tiles in batches:
-        outputs = cnn(gpu_tiles)
-        softmaxed = softmax(outputs, 1)
-        foreground_probs = softmaxed[:, 1, :]  # just the foreground probability.
-        if threshold is not None:
-            predicted = foreground_probs > threshold
-            predicted = predicted.view(-1).int()
-        else:
-            predicted = foreground_probs
-
-        pred_np = predicted.data.cpu().numpy()
-        out_tiles = pred_np.reshape((len(gpu_tiles), out_w, out_w))
-        for out_tile in out_tiles:
-            output_tiles.append(out_tile)
-
-    assert len(output_tiles) == len(coords), (
-        f'{len(output_tiles)} {len(coords)}')
-
-    reconstructed = im_utils.reconstruct_from_tiles(output_tiles, coords,
-                                                    image.shape[:-1])
-    return reconstructed
-
-
-
-'''
-====================================================================================================
-'''
-
-def image_and_segmentation(imageDir, imageSegDir):
-    image = im_utils.load_image(imageDir)
-    imageSeg = imread(imageSegDir)
-
-    '''
-
-    imageVitSeg=np.zeros([(image).shape[0],(image).shape[1],4])
-
-    for x in range(image.shape[0]): 
-        for y in range(image.shape[1]):
-            for z in range(image.shape[2]):
-                imageVitSeg[x][y][z]=image[x][y][z]
-
-            imageVitSeg[x][y][3]=imageSeg[x][y][3]
-
-            if (imageSeg[x][y][3] != 0):
-                imageVitSeg[x][y][3]=1
-    
-    '''
-    imageSegBul = np.array(imageSeg[:,:,2:3]) #take the segmen imiget and set it to one layer 
-    imageVitSeg = np.concatenate((image,imageSegBul), axis=2) # concat imig and segmentaysen to 4 layer
-
-    #im_utils.save_then_move(saveDir, imageVitSeg)
-    
-    image = img_as_float32(image)
-    imageSegBul = np.array(imageSeg[:,:,2:3], dtype=bool)
-    imageVitSeg = np.concatenate((image,imageSegBul), axis=2)
-    
-    return imageVitSeg
-
-
-
-def dif_seg_ann(imageSegDir, imageAnnDir, imageSaveDir):
-    imageSeg = imread(imageSegDir) #shut be models model segrigation /home/jonatan/Documents/diku/BA/testbil/sek/B85-1_000.png
-    imageAnn = imread(imageAnnDir)
-
-    imageAnnAnn=np.zeros([(imageAnn).shape[0],(imageAnn).shape[1],4])
-
-    for x in range(imageAnn.shape[0]): 
-        for y in range(imageAnn.shape[1]):
-
-            if (imageSeg[x][y][3] and not (imageAnn[x][y][3])): #false posetiv
-                imageAnnAnn[x][y][1] = 255
-                imageAnnAnn[x][y][3] = 180
-
-            if (not (imageSeg[x][y][3]) and imageAnn[x][y][3]): #False negativ
-                imageAnnAnn[x][y][0] = 255
-                imageAnnAnn[x][y][3] = 180
-
-    im_utils.save_then_move(imageSaveDir, imageAnnAnn)
-    pass
-
-
-
-def dif_new_ann(imageSegDir, imageAnnDir):
-    imageSeg = imread(imageSegDir) #shut be models model segrigation /home/jonatan/Documents/diku/BA/testbil/sek/B85-1_000.png
-    imageAnn = imread(imageAnnDir)
-
-    imageAnnAnn=np.zeros([(imageAnn).shape[0],(imageAnn).shape[1],4], np.uint8)
-
-    for x in range(imageAnn.shape[0]): 
-        for y in range(imageAnn.shape[1]):
-
-            if ((imageSeg[x][y][3] and imageAnn[x][y][0]) or (not (imageSeg[x][y][3]) and imageAnn[x][y][1])):
-                imageAnnAnn[x][y][1] = 255
-                imageAnnAnn[x][y][3] = 180
-
-            elif (imageAnn[x][y][3]):
-                imageAnnAnn[x][y][0] = 255
-                imageAnnAnn[x][y][3] = 180
-
-            else:
-                imageAnnAnn[x][y][1] = 255
-                imageAnnAnn[x][y][3] = 180
-    return imageAnnAnn.astype('uint8')
-
-
-
-def test_new_model(out_path):
-    #make model 
-    #models=load_model(syncdir+project+'/models_models/000001_1659961126.pkl')
-
-    imagedir = syncdir+datasets+'/B1-1_000.jpg'
-    imageSegDir = syncdir+project+segmentations+ '/B1-1_000.png'
-
-    image = image_and_segmentation(imagedir,imageSegDir)
-    #image = im_utils.load_image(imagedir)
-
-    #load imig
-    #B9-1_002.png
-    #B1-1_000.png god
-
-    tiles, coords = im_utils.get_tiles(image, in_tile_shape=(in_w, in_w, 4), out_tile_shape=(out_w, out_w))
-
-
-    #segmented = ensemble_segment([syncdir+project+'/models_models/000001_1659961126.pkl'], image, bs, in_w, out_w)
-    segmented = ensemble_segment([syncdir+project+'/models_models/000001_2_1660065013.pkl'], image, bs, in_w, out_w)
-
+def gradian_data_setop(model_paths):
+    image=datasets+'/B85-1_000.jpg'
+    segmented = segment_gradian(model_paths, image, bs, in_w, out_w)
 
     seg_alpha = np.zeros((segmented.shape[0], segmented.shape[1], 4))
     seg_alpha[segmented > 0] = [0, 1.0, 1.0, 0.7]
 
     seg_alpha  = (seg_alpha * 255).astype(np.uint8)
 
-    im_utils.save_then_move(out_path, seg_alpha)
-    pass
-
-
-
-def setup(setup_dir):
-    os.mkdir(setup_dir +'/models_models')
-    os.mkdir(setup_dir +'/models_models' + '/data')
-    os.mkdir(setup_dir +'/models_models' + '/seg')
-    os.mkdir(setup_dir +'/models_models' + '/annotations')
-    os.mkdir(setup_dir +'/models_models' + '/annotations/train')
-    os.mkdir(setup_dir +'/models_models' + '/annotations/val')
-    pass
-
-def setup_date(setup_dir):
-    #fnames = ls(setup_dir + segmentations)
-    #fnames = [a for a in fnames if im_utils.is_photo(a)]
-
-    #for fname in fnames:
-    #    Dataimig = image_and_segmentation(syncdir+datasets+'/' + os.path.splitext(fname)[0] + '.jpg' ,setup_dir +'/segmentations/' + fname)
-    #    imsave(setup_dir + '/models_models/data/'+fname, Dataimig)
-
-    '''
-    val = '/annotations/val'
-    train = '/annotations/train'
-    '''
-
-    fnames = ls(setup_dir + val)
-    fnames = [a for a in fnames if im_utils.is_photo(a)]
-
-    for fname in fnames:
-        #dif_new_ann(imageSegDir, imageAnnDir)
-        DataImig = dif_new_ann(setup_dir +'/segmentations/' + fname ,setup_dir +'/annotations/val/' + fname)
-        imsave(setup_dir + '/models_models/annotations/val/'+fname, DataImig)
-
-
-    fnames = ls(setup_dir + train)
-    fnames = [a for a in fnames if im_utils.is_photo(a)]
-
-    for fname in fnames:
-        #dif_new_ann(imageSegDir, imageAnnDir)
-        DataImig = dif_new_ann(setup_dir +'/segmentations/' + fname ,setup_dir +'/annotations/train/' + fname)
-        imsave(setup_dir + '/models_models/annotations/train/'+fname, DataImig)
-
-    pass
-
-
-
-def train_one_epoch(train_set,model, optimizer):
-    
-    model.train()
-
-
-    train_loader = DataLoader(train_set, bs, shuffle=True,
-                              # 12 workers is good for performance
-                              # on 2 RTX2080 Tis
-                              # 0 workers is good for debugging
-                              # don't go above 12 workers and don't go above the number of cpus
-                              num_workers=min(multiprocessing.cpu_count(), 12),
-                              drop_last=False, pin_memory=True)
-
-    num_of_traning_no_better=0
-
-    while num_of_traning_no_better<10:
-
-        for step, (photo_tiles,
-               foreground_tiles,
-               defined_tiles) in enumerate(train_loader):
-
-
-            photo_tiles = photo_tiles.cuda()
-            foreground_tiles = foreground_tiles.cuda()
-            defined_tiles = defined_tiles.cuda()
-
-            optimizer.zero_grad()
-
-            outputs = model(photo_tiles)
-            softmaxed = softmax(outputs, 1)
-
-            foreground_probs = softmaxed[:, 1, :]
-
-            outputs[:, 0] *= defined_tiles
-            outputs[:, 1] *= defined_tiles
-
-            loss = criterion(outputs, foreground_tiles)
-            loss.backward()
-            optimizer.step()
-
-            if (step>9):
-                pass
-
-        if validation(model):
-            num_of_traning_no_better = 0
-        else:
-            num_of_traning_no_better = num_of_traning_no_better+1
-
-
-def validation(model):
-
-    get_val_metrics = partial(models_model_lib.get_val_metrics,
-                          val_annot_dir=syncdir+project+'/models_models'+val,
-                          dataset_dir=syncdir+project+'/models_models/data',
-                          in_w=in_w, out_w=out_w, bs=bs)
-
-
-    model_dir=syncdir+project+'/models_models/models'
-    prev_path = model_utils.get_latest_model_paths(model_dir, k=1)[0]
-    prev_model =load_model(prev_path)
-
-    cur_metrics = get_val_metrics(copy.deepcopy(model))
-    prev_metrics = get_val_metrics(prev_model)
-
-    print('cur_metrics')
-    print(cur_metrics)
-
-    print('prev_metrics')
-    print(prev_metrics)
-
-    
-    was_saved = save_if_better(model_dir, model, prev_path,
-                           cur_metrics['f1'], prev_metrics['f1'])
-
-    return was_saved
-    
-
-
-def train_type2(model_path, train_annot_dir, dataset_dir):
-    train_set = TrainDataset2(train_annot_dir,dataset_dir,in_w,out_w)
-
-    model = load_model(model_path)
-
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.99, nesterov=True)
-
-    train_one_epoch(train_set, model, optimizer)    
+    im_utils.save_then_move(syncdir+project+'/test.png', seg_alpha)
     pass
 
 '''
@@ -405,13 +87,16 @@ val = '/annotations/val'
 train = '/annotations/train'
 
 #print(syncdir+datasets+'/B85-1_000.png')
-print(syncdir+project+'/models_models')
+print(syncdir+project+'/models_models/000015_1578333385.pkl')
 
+gradian_data_setop(syncdir+project+'/models/')
 
 #train_type2(model_path, train_annot_dir, dataset_dir)
+'''
 train_type2(syncdir+project+'/models_models/models/000003_1661779227.pkl'
     , syncdir+project+train
     , syncdir+project+'/models_models/data')
+'''
 
 #model=load_model(syncdir+project+'/models_models/models/000001_1661772775.pkl')
 
--- trainer/models_model_lib.py
+++ trainer/models_model_lib.py
@@ -1,54 +1,9 @@
 
-'''
-import torch
-from torch import nn
-from torch.nn import Parameter
-
-def test_opt():
-
-#    x = torch.tensor([1,2,3,4,5,6,7,8,9,10])
-#    y = torch.tensor([10,11,12,13,14,15,16,17,18,19], dtype=torch.float)
-
-    x = torch.tensor([1,2,3,4,5])
-    y = torch.tensor([11,12,13,14,15], dtype=torch.float)
-
-    # model
-
-    a = torch.randn(1, requires_grad=True, dtype=torch.float)
-    b = torch.randn(1, requires_grad=True, dtype=torch.float)
-
-
-    print(a,b)
-    model = [Parameter(a),Parameter(b)]
-
-    lr=0.05
-    criterion = nn.MSELoss()
-    optimaser = torch.optim.SGD(model, lr=lr)
-
-
-    for epoch in range(20):
-        optimaser.zero_grad()
-
-        y_p=model[0]+model[1]*x
-
-        loss = criterion(y_p, y)
-
-        loss.backward()
-
-        optimaser.step()
-
-        print(loss)
-
-    print(x,y,y_p)
-    print(model[0],model[1])
-
-test_opt()
-'''
-
-
 import model_utils
 import im_utils
 from unet2 import UNetGNRes
+
+import multiprocessing
 
 import time
 import os
@@ -57,7 +12,12 @@ from skimage.io import imread, imsave
 from skimage import color, img_as_float32
 import torch
 from torch.nn.functional import softmax
+from loss import combined_loss as criterion
+from torch.utils.data import DataLoader
 from file_utils import ls
+from model_utils import save_if_better
+from functools import partial
+import copy
 
 import glob
 import shutil
@@ -144,9 +104,8 @@ def unet_segment(cnn, image, bs, in_w, out_w, threshold=0.5):
         for _ in range(bs):
             if tile_idx < len(tiles):
                 tile = tiles[tile_idx]
-                print('shape(tile)')
-                print(np.shape(tile))
-                #tile = img_as_float32(tile)
+
+                tile = as_float32(tile)
                 tile = im_utils.normalize_tile(tile)
                 #print(tile)
                 tile = np.moveaxis(tile, -1, 0)
@@ -309,3 +268,247 @@ def load_train_image_and_annot(dataset_dir, train_annot_dir):
 '''
 ====================================================================================================
 '''
+
+'''
+my funtion
+'''
+def as_float32(image)#img_as_float32 for imige with seg
+
+        image_seg = np.array(image[:,:,3:])/255
+        image_RGB = np.array(image[:,:,:3])
+
+        image_RGB = img_as_float32(image_RGB)
+        image = np.concatenate((image_RGB,image_seg), axis=2)
+
+        return image
+
+
+def image_and_segmentation(imageDir, imageSegDir):
+    image = im_utils.load_image(imageDir)
+    imageSeg = imread(imageSegDir)
+
+    '''
+
+    imageVitSeg=np.zeros([(image).shape[0],(image).shape[1],4])
+
+    for x in range(image.shape[0]): 
+        for y in range(image.shape[1]):
+            for z in range(image.shape[2]):
+                imageVitSeg[x][y][z]=image[x][y][z]
+
+            imageVitSeg[x][y][3]=imageSeg[x][y][3]
+
+            if (imageSeg[x][y][3] != 0):
+                imageVitSeg[x][y][3]=1
+    
+    '''
+    imageSegBul = np.array(imageSeg[:,:,2:3]) #take the segmen imiget and set it to one layer 
+    imageVitSeg = np.concatenate((image,imageSegBul), axis=2) # concat imig and segmentaysen to 4 layer
+
+    #im_utils.save_then_move(saveDir, imageVitSeg)
+    
+    image = img_as_float32(image)
+    imageSegBul = np.array(imageSeg[:,:,2:3], dtype=bool)
+    imageVitSeg = np.concatenate((image,imageSegBul), axis=2)
+    
+    return imageVitSeg
+
+
+
+def dif_seg_ann(imageSegDir, imageAnnDir, imageSaveDir):
+    imageSeg = imread(imageSegDir) #shut be models model segrigation /home/jonatan/Documents/diku/BA/testbil/sek/B85-1_000.png
+    imageAnn = imread(imageAnnDir)
+
+    imageAnnAnn=np.zeros([(imageAnn).shape[0],(imageAnn).shape[1],4])
+
+    for x in range(imageAnn.shape[0]): 
+        for y in range(imageAnn.shape[1]):
+
+            if (imageSeg[x][y][3] and not (imageAnn[x][y][3])): #false posetiv
+                imageAnnAnn[x][y][1] = 255
+                imageAnnAnn[x][y][3] = 180
+
+            if (not (imageSeg[x][y][3]) and imageAnn[x][y][3]): #False negativ
+                imageAnnAnn[x][y][0] = 255
+                imageAnnAnn[x][y][3] = 180
+
+    im_utils.save_then_move(imageSaveDir, imageAnnAnn)
+    pass
+
+
+
+def dif_new_ann(imageSegDir, imageAnnDir):
+    imageSeg = imread(imageSegDir) #shut be models model segrigation /home/jonatan/Documents/diku/BA/testbil/sek/B85-1_000.png
+    imageAnn = imread(imageAnnDir)
+
+    imageAnnAnn=np.zeros([(imageAnn).shape[0],(imageAnn).shape[1],4], np.uint8)
+
+    for x in range(imageAnn.shape[0]): 
+        for y in range(imageAnn.shape[1]):
+
+            if ((imageSeg[x][y][3] and imageAnn[x][y][0]) or (not (imageSeg[x][y][3]) and imageAnn[x][y][1])):
+                imageAnnAnn[x][y][1] = 255
+                imageAnnAnn[x][y][3] = 180
+
+            elif (imageAnn[x][y][3]):
+                imageAnnAnn[x][y][0] = 255
+                imageAnnAnn[x][y][3] = 180
+
+            else:
+                imageAnnAnn[x][y][1] = 255
+                imageAnnAnn[x][y][3] = 180
+    return imageAnnAnn.astype('uint8')
+
+
+
+def test_new_model(out_path):
+    #make model 
+    #models=load_model(syncdir+project+'/models_models/000001_1659961126.pkl')
+
+    imagedir = syncdir+datasets+'/B1-1_000.jpg'
+    imageSegDir = syncdir+project+segmentations+ '/B1-1_000.png'
+
+    image = image_and_segmentation(imagedir,imageSegDir)
+    #image = im_utils.load_image(imagedir)
+
+    #load imig
+    #B9-1_002.png
+    #B1-1_000.png god
+
+    tiles, coords = im_utils.get_tiles(image, in_tile_shape=(in_w, in_w, 4), out_tile_shape=(out_w, out_w))
+
+
+    #segmented = ensemble_segment([syncdir+project+'/models_models/000001_1659961126.pkl'], image, bs, in_w, out_w)
+    segmented = ensemble_segment([syncdir+project+'/models_models/000001_2_1660065013.pkl'], image, bs, in_w, out_w)
+
+
+    seg_alpha = np.zeros((segmented.shape[0], segmented.shape[1], 4))
+    seg_alpha[segmented > 0] = [0, 1.0, 1.0, 0.7]
+
+    seg_alpha  = (seg_alpha * 255).astype(np.uint8)
+
+    im_utils.save_then_move(out_path, seg_alpha)
+    pass
+
+
+
+def setup(setup_dir):
+    os.mkdir(setup_dir +'/models_models')
+    os.mkdir(setup_dir +'/models_models' + '/data')
+    os.mkdir(setup_dir +'/models_models' + '/seg')
+    os.mkdir(setup_dir +'/models_models' + '/annotations')
+    os.mkdir(setup_dir +'/models_models' + '/annotations/train')
+    os.mkdir(setup_dir +'/models_models' + '/annotations/val')
+    pass
+
+def setup_date(setup_dir):
+    fnames = ls(setup_dir + segmentations)
+    fnames = [a for a in fnames if im_utils.is_photo(a)]
+
+    for fname in fnames:
+        Dataimig = image_and_segmentation(syncdir+datasets+'/' + os.path.splitext(fname)[0] + '.jpg' ,setup_dir +'/segmentations/' + fname)
+        imsave(setup_dir + '/models_models/data/'+fname, Dataimig)
+
+    '''
+    val = '/annotations/val'
+    train = '/annotations/train'
+    '''
+
+    fnames = ls(setup_dir + val)
+    fnames = [a for a in fnames if im_utils.is_photo(a)]
+
+    for fname in fnames:
+        #dif_new_ann(imageSegDir, imageAnnDir)
+        DataImig = dif_new_ann(setup_dir +'/segmentations/' + fname ,setup_dir +'/annotations/val/' + fname)
+        imsave(setup_dir + '/models_models/annotations/val/'+fname, DataImig)
+
+
+    fnames = ls(setup_dir + train)
+    fnames = [a for a in fnames if im_utils.is_photo(a)]
+
+    for fname in fnames:
+        #dif_new_ann(imageSegDir, imageAnnDir)
+        DataImig = dif_new_ann(setup_dir +'/segmentations/' + fname ,setup_dir +'/annotations/train/' + fname)
+        imsave(setup_dir + '/models_models/annotations/train/'+fname, DataImig)
+
+    pass
+
+
+
+def train_epoch(train_set,model, optimizer):
+    
+    model.train()
+
+
+    train_loader = DataLoader(train_set, bs, shuffle=True,
+                              # 12 workers is good for performance
+                              # on 2 RTX2080 Tis
+                              # 0 workers is good for debugging
+                              # don't go above 12 workers and don't go above the number of cpus
+                              num_workers=min(multiprocessing.cpu_count(), 12),
+                              drop_last=False, pin_memory=True)
+
+    num_of_traning_no_better=0
+
+    while num_of_traning_no_better<10:
+
+        for step, (photo_tiles,
+               foreground_tiles,
+               defined_tiles) in enumerate(train_loader):
+
+
+            photo_tiles = photo_tiles.cuda()
+            foreground_tiles = foreground_tiles.cuda()
+            defined_tiles = defined_tiles.cuda()
+
+            optimizer.zero_grad()
+
+            outputs = model(photo_tiles)
+            softmaxed = softmax(outputs, 1)
+
+            foreground_probs = softmaxed[:, 1, :]
+
+            outputs[:, 0] *= defined_tiles
+            outputs[:, 1] *= defined_tiles
+
+            loss = criterion(outputs, foreground_tiles)
+            loss.backward()
+            optimizer.step()
+
+            if (step>9):
+                pass
+
+        if validation(model):
+            num_of_traning_no_better = 0
+        else:
+            num_of_traning_no_better = num_of_traning_no_better+1
+
+
+def validation(model):
+
+    get_val_metrics = partial(models_model_lib.get_val_metrics,
+                          val_annot_dir=syncdir+project+'/models_models'+val,
+                          dataset_dir=syncdir+project+'/models_models/data',
+                          in_w=in_w, out_w=out_w, bs=bs)
+
+
+    model_dir=syncdir+project+'/models_models/models'
+    prev_path = model_utils.get_latest_model_paths(model_dir, k=1)[0]
+    prev_model =load_model(prev_path)
+
+    cur_metrics = get_val_metrics(copy.deepcopy(model))
+    prev_metrics = get_val_metrics(prev_model)
+
+    print('cur_metrics')
+    print(cur_metrics)
+
+    print('prev_metrics')
+    print(prev_metrics)
+
+    
+    was_saved = save_if_better(model_dir, model, prev_path,
+                           cur_metrics['f1'], prev_metrics['f1'])
+
+    return was_saved
+    
+
