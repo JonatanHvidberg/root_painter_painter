78778fd88b28fc8d65f0d3dcbb284064ecc26c3b
JonatanHvidberg <jonatan@eksperimentrum.dk>
2022-09-07 13:33:27 +0200

    gerdian

--- trainer/models_model.py
+++ trainer/models_model.py
@@ -331,7 +331,7 @@ def coler_gradian(segmented):
 
     for x in range(segmented.shape[0]):
         for y in range(segmented.shape[1]):
-            seg_alpha[x][y] = [1-segmented[x][y], segmented[x][y], segmented[x][y], 0.7]
+            seg_alpha[x][y] = [1-segmented[x][y], segmented[x][y], 0, 0.7]
             if segmented[x][y]>0.5:
                 print(segmented[x][y])
             
@@ -353,7 +353,7 @@ def result():
 
     gradian = coler_gradian(seg)
 
-    #imsave(syncdir+project+'/models_models/gB44-1_003.png', gradian)
+    imsave(syncdir+project+'/models_models/gB44-1_003.png', gradian)
 
 '''
 Data
