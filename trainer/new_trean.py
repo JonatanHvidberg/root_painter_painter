2e25f0c2bb50c57220851e6b22686865463c5758
JonatanHvidberg <jonatan@eksperimentrum.dk>
2022-10-12 10:56:22 +0200

    model tran

--- trainer/new_trean.py
+++ trainer/new_trean.py
@@ -109,6 +109,25 @@ val = '/labels/val'
 train = '/labels/train'
 test = '/labels/test'
 
+
+global in_w
+global out_w
+global mem_per_item
+global total_mem
+global bs
+
+in_w = 572
+out_w = 500
+mem_per_item = 3800000000
+total_mem = 0
+print('GPU Available', torch.cuda.is_available())
+for i in range(torch.cuda.device_count()):
+    total_mem += torch.cuda.get_device_properties(i).total_memory
+bs = total_mem // mem_per_item
+bs = min(12, bs)
+print('Batch size', bs)
+
+
 for x in range(2,6):
     mml.create_first_model_with_random_weights(syncdir+project+'/models_models/models'+str(x)+'/')
     train_type2(syncdir+project+'/models_models/models'+str(x)+'/'
