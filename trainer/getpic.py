21cf666fb33f5a9e67c6ace5546e2bda811f4bca
JonatanHvidberg <jonatan@eksperimentrum.dk>
2022-10-27 11:18:06 +0200

    virk forhelvet

--- trainer/getpic.py
+++ trainer/getpic.py
@@ -28,6 +28,7 @@ def result(fname,n):
 
     image = imread(syncdir+project+'/models_models/data/'+fname)
     seg = mml.simbel_segment(model,image)
+    print('result', np.sum(seg))
     gradian = coler_gradian(seg)
     imsave(syncdir+project+'/models_models/'+ fname, gradian)
 
@@ -37,6 +38,7 @@ def result_unsertensu(fname,om):
 
     image = im_utils.load_image(syncdir+project+'/models_models/data/'+fname)
     seg = mml.simbel_segment_unsertensu(model,image)
+    print('unsertensu', np.sum(seg))
     gradian = coler_gradian(seg)
     imsave(syncdir+project+'/models_models/u_'+ fname, gradian)
 
@@ -45,12 +47,14 @@ def both(fname,n,om):
     result_unsertensu(fname,om)
        
 syncdir='drive_rp_sync/projects/'
-
+'''
 n='5'
 om='000032_1578339309.pkl'
 project='biopores_a_corrective'
 both('B58-1_002.png',n,om)
 both('B100-1_002.png',n,om)
+both('B1-1_000.png',n,om)
+
 
 n='3'
 om='000022_1578319359.pkl'
@@ -63,16 +67,17 @@ om='000028_1581172999.pkl'
 project='nodules_a_corrective'
 both('081_001.png',n,om)
 both('053_003.png',n,om)
-
+'''
 n='5'
 om='000023_1581690809.pkl'#last model
 project='nodules_b_corrective'
 both('075_000.png',n,om)
 both('074_001.png',n,om)
-
+'''
 n='1'
 om='000046_1578155544.pkl'
 project='towers_a_corrective'
+both('16_07_18_12E2d_P7181771_000.png',n,om)
 both('16_07_04_10E5b_P7041084_000.png',n,om)
 both('16_07_04_10E5b_P7041084_000.png',n,om)
 
@@ -87,3 +92,4 @@ om='000047_1635794623.pkl'
 project='rg_2017_ags'
 both('Radimax_Cam4_PipeID_524_Position_2747_TS_2017-07-06 09.47.48.866F.bdiv.rgb_000.png',n,om)
 both('Radimax_Cam4_PipeID_524_Position_2747_TS_2017-07-06 09.47.48.866F.bdiv.rgb_000.png',n,om)
+'''
